2407015ae023be8082bc06b32c21411bfd39ebd1
zhangyongwei <694498731@qq.com>
2017-03-26 23:13:57 +0800

    10.添加播放器

---
+++ app/src/main/java/com/yongweizhang/bilibili/activity/DanmkuVideoActivity.java
@@ -0,0 +1,187 @@
+package com.yongweizhang.bilibili.activity;
+
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.widget.NestedScrollView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.shuyu.gsyvideoplayer.GSYPreViewManager;
+import com.shuyu.gsyvideoplayer.GSYVideoPlayer;
+import com.shuyu.gsyvideoplayer.listener.LockClickListener;
+import com.shuyu.gsyvideoplayer.utils.OrientationUtils;
+import com.shuyu.gsyvideoplayer.video.StandardGSYVideoPlayer;
+import com.yongweizhang.bilibili.R;
+import com.yongweizhang.bilibili.base.SampleListener;
+
+import butterknife.InjectView;
+
+public class DanmkuVideoActivity extends AppCompatActivity {
+
+    @InjectView(R.id.post_detail_nested_scroll)
+    NestedScrollView postDetailNestedScroll;
+    @InjectView(R.id.danmaku_player)
+    DanmakuVideoPlayer danmakuPlayer;
+    @InjectView(R.id.activity_detail_player)
+    RelativeLayout activityDetailPlayer;
+
+    private boolean isPlay;
+    private boolean isPause;
+
+    private OrientationUtils orientationUtils;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_danmku_video);
+
+        //使用自定义的全屏切换图片，!!!注意xml布局中也需要设置为一样的
+        //必须在setUp之前设置
+        danmakuPlayer.setShrinkImageRes(R.drawable.custom_shrink);
+        danmakuPlayer.setEnlargeImageRes(R.drawable.custom_enlarge);
+
+        String url = "http://baobab.wdjcdn.com/14564977406580.mp4";
+        //String url = "https://res.exexm.com/cw_145225549855002";
+        danmakuPlayer.setUp(url, true, null, "测试视频");
+
+        //增加封面
+        ImageView imageView = new ImageView(this);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setImageResource(R.mipmap.xxx1);
+        danmakuPlayer.setThumbImageView(imageView);
+
+        resolveNormalVideoUI();
+
+        //外部辅助的旋转，帮助全屏
+        orientationUtils = new OrientationUtils(this, danmakuPlayer);
+        //初始化不打开外部的旋转
+        orientationUtils.setEnable(false);
+
+        danmakuPlayer.setIsTouchWiget(true);
+        //关闭自动旋转
+        danmakuPlayer.setRotateViewAuto(false);
+        danmakuPlayer.setLockLand(false);
+        danmakuPlayer.setShowFullAnimation(false);
+        danmakuPlayer.setNeedLockFull(true);
+
+        //detailPlayer.setOpenPreView(true);
+        danmakuPlayer.getFullscreenButton().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //直接横屏
+                orientationUtils.resolveByClick();
+
+                //第一个true是否需要隐藏actionbar，第二个true是否需要隐藏statusbar
+                danmakuPlayer.startWindowFullscreen(DanmkuVideoActivity.this, true, true);
+            }
+        });
+
+        danmakuPlayer.setStandardVideoAllCallBack(new SampleListener() {
+            @Override
+            public void onPrepared(String url, Object... objects) {
+                super.onPrepared(url, objects);
+                //开始播放了才能旋转和全屏
+                orientationUtils.setEnable(true);
+                isPlay = true;
+            }
+
+            @Override
+            public void onAutoComplete(String url, Object... objects) {
+                super.onAutoComplete(url, objects);
+            }
+
+            @Override
+            public void onClickStartError(String url, Object... objects) {
+                super.onClickStartError(url, objects);
+            }
+
+            @Override
+            public void onQuitFullscreen(String url, Object... objects) {
+                super.onQuitFullscreen(url, objects);
+                if (orientationUtils != null) {
+                    orientationUtils.backToProtVideo();
+                }
+            }
+        });
+
+        danmakuPlayer.setLockClickListener(new LockClickListener() {
+            @Override
+            public void onClick(View view, boolean lock) {
+                if (orientationUtils != null) {
+                    //配合下方的onConfigurationChanged
+                    orientationUtils.setEnable(!lock);
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (orientationUtils != null) {
+            orientationUtils.backToProtVideo();
+        }
+
+        if (StandardGSYVideoPlayer.backFromWindowFull(this)) {
+            return;
+        }
+        super.onBackPressed();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        isPause = true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        isPause = false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        GSYVideoPlayer.releaseAllVideos();
+        GSYPreViewManager.instance().releaseMediaPlayer();
+        if (orientationUtils != null)
+            orientationUtils.releaseListener();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        //如果旋转了就全屏
+        if (isPlay && !isPause) {
+            if (newConfig.orientation == ActivityInfo.SCREEN_ORIENTATION_USER) {
+                if (!danmakuPlayer.isIfCurrentIsFullscreen()) {
+                    danmakuPlayer.startWindowFullscreen(DanmkuVideoActivity.this, true, true);
+                }
+            } else {
+                //新版本isIfCurrentIsFullscreen的标志位内部提前设置了，所以不会和手动点击冲突
+                if (danmakuPlayer.isIfCurrentIsFullscreen()) {
+                    StandardGSYVideoPlayer.backFromWindowFull(this);
+                }
+                if (orientationUtils != null) {
+                    orientationUtils.setEnable(true);
+                }
+            }
+        }
+    }
+
+
+    private void resolveNormalVideoUI() {
+        //增加title
+        danmakuPlayer.getTitleTextView().setVisibility(View.GONE);
+        danmakuPlayer.getTitleTextView().setText("测试视频");
+        danmakuPlayer.getBackButton().setVisibility(View.GONE);
+    }
+
+}
--- app/src/main/java/com/yongweizhang/bilibili/activity/TopicWebActivity.java
+++ app/src/main/java/com/yongweizhang/bilibili/activity/TopicWebActivity.java
@@ -1,36 +1,43 @@
 package com.yongweizhang.bilibili.activity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.yongweizhang.bilibili.R;
 import com.yongweizhang.bilibili.adapter.TopicAdapter;
+import com.yongweizhang.bilibili.utils.ClipboardUtil;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 public class TopicWebActivity extends AppCompatActivity {
 
-    @InjectView(R.id.iv_back)
-    ImageView ivBack;
-    @InjectView(R.id.tv_text)
-    TextView tvText;
-    @InjectView(R.id.iv_more)
-    ImageView ivMore;
+
+    @InjectView(R.id.share_toolbar)
+    Toolbar shareToolbar;
+    @InjectView(R.id.app_bar_layout)
+    AppBarLayout appBarLayout;
     @InjectView(R.id.wb_info_more)
     WebView wbInfoMore;
     @InjectView(R.id.activity_live_info)
     LinearLayout activityLiveInfo;
+    private Intent intent;
+    private String titles;
+    private String link;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,26 +45,35 @@ public class TopicWebActivity extends AppCompatActivity {
         setContentView(R.layout.activity_topic_web);
         ButterKnife.inject(this);
 
+        initData();
         loadWeb();
-        initLinstener();
+    }
+
+    private void initData() {
+
+        String titles = getIntent().getStringExtra(TopicAdapter.KEY);
+        String link = getIntent().getStringExtra(TopicAdapter.VALUE);
+
+        if (!TextUtils.isEmpty(link)) {
+            wbInfoMore.loadUrl(link);
+        }
+        shareToolbar.setTitle(TextUtils.isEmpty(titles) ? "详情" : titles);
+
+        setSupportActionBar(shareToolbar);
+        ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            supportActionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
     }
 
     private void initLinstener() {
 
-        ivBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+
     }
 
 
     private void loadWeb() {
-
-
-        String title = getIntent().getStringExtra(TopicAdapter.KEY);
-        String link = getIntent().getStringExtra(TopicAdapter.VALUE);
 
 
         WebSettings webSettings = wbInfoMore.getSettings();
@@ -86,7 +102,7 @@ public class TopicWebActivity extends AppCompatActivity {
 
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-
+                //判断新版本
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
 
                     view.loadUrl(request.getUrl().toString());
@@ -96,15 +112,69 @@ public class TopicWebActivity extends AppCompatActivity {
             }
         });
 
-        if(!TextUtils.isEmpty(link)) {
+
+
+        if (!TextUtils.isEmpty(link)) {
             wbInfoMore.loadUrl(link);
 
         }
-        if(!TextUtils.isEmpty(title)) {
-
-            tvText.setText(title);
-        }
+//        if (!TextUtils.isEmpty(title)) {
+//
+//            tvText.setText(title);
+//        }
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.topic_share, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+            case R.id.menu_share:
+                share();
+                break;
+            case R.id.menu_browseropen:
+                intent = new Intent(Intent.ACTION_VIEW);
+                startActivity(intent);
+                break;
+            case R.id.menu_copylink:
+                ClipboardUtil.setText(TopicWebActivity.this, link);
+                Toast.makeText(this, "已复制", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //分享法
+    private void share() {
+        intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "分享");
+        intent.putExtra(Intent.EXTRA_TEXT, "来自「哔哩哔哩」的分享:" + link);
+        startActivity(Intent.createChooser(intent, titles));
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (wbInfoMore.canGoBack() && wbInfoMore.copyBackForwardList().getSize() > 0
+                && !wbInfoMore.getUrl().equals(wbInfoMore.copyBackForwardList().getItemAtIndex(0)
+                .getOriginalUrl())) {
+            wbInfoMore.goBack();
+        } else {
+            finish();
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        wbInfoMore.destroy();
+        super.onDestroy();
+    }
 }
